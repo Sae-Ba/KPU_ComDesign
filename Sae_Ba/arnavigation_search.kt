f430f1c73855b991d89d4575aed1b866ad3456e8
JiHoon Choi <69957419+Sae-Ba@users.noreply.github.com>
2022-03-22 21:49:11 +0900

    Add files via upload

---
+++ Sae_Ba/arnavigation_search.kt
@@ -0,0 +1,11 @@
+package com.tuk.tukar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Arnavigation_search : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_arnavigation_search)
+    }
+}
---
+++ Sae_Ba/non_member_menu.kt
@@ -0,0 +1,11 @@
+package com.tuk.tukar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Non_member_menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_non_member_menu)
+    }
+}
