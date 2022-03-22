55eeb6b9a6b902ed271d07c83e5705cbc48fc70a
JiHoon Choi <69957419+Sae-Ba@users.noreply.github.com>
2022-03-22 22:24:28 +0900

    Update member_menu.kt

--- Sae_Ba/member_menu.kt
+++ Sae_Ba/member_menu.kt
@@ -1,11 +1,21 @@
 package com.tuk.tukar
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tuk.tukar.databinding.ActivityMemberMenuBinding
+
 
 class Member_menu : AppCompatActivity() {
+    private lateinit var binding: ActivityMemberMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_member_menu)
+        binding = ActivityMemberMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val intent = Intent(this, Arnavigation_search::class.java)
+            startActivity(intent)
+        }
     }
 }
